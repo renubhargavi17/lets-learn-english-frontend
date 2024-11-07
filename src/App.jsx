059a961dc01b322319a4b7f42b8add9a91193ae0
bzhang98 <105823390+bzhang98@users.noreply.github.com>
2024-11-07 14:13:57 -0600

    add route placeholders

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,34 @@
-
-import './App.scss'
-import Language from './components/Language/Language'
-import Header from './components/Header/Header.jsx'
-
-
+import "./App.scss";
+import Language from "./components/Language/Language";
+import Header from "./components/Header/Header.jsx";
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
- 
+  const [language, setLanguage] = useState(null);
+  const [difficulty, setDifficulty] = useState(null);
 
   return (
- <div  className='learnLanguage'>
-    <Header/>
+    <BrowserRouter>
+      <div className="learnLanguage">
+        <Header />
 
-      <Language/>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Language setLanguage={setLanguage} />} />
+            <Route
+              path="/difficulty"
+              element={<Language setLanguage={setLanguage} />}
+            />
+            <Route
+              path="/quiz"
+              element={<Language setLanguage={setLanguage} />}
+            />
+          </Routes>
+        </Router>
       </div>
-  
-  
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
