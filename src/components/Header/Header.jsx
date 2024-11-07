5d2ef00e3b1371ceba80e7faceffbc8d3ad2a839
Renu Bhargavi Seetharaman <renubhargavi17@gmail.com>
2024-11-07 15:07:02 -0500

    level branch changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ function Header(){
     return(
         <header className="header">
         <h1 className="title">LET'S LEARN ENGLISH</h1>
-        <h2 className="heading">WHAT LANGUAGE DO YOU SPEAK?</h2>
+        
         </header>
     )
 }
---
+++ src/components/Level/Level.jsx
@@ -0,0 +1,24 @@
+import "../Level/Level.scss"
+
+function Level(){
+    return(
+        <section className="languageLevel">
+       
+        <h2 className="heading">YOU SPEAK: languagetype</h2>
+        <div className="levelType">
+            <div className="easyLevel">
+                <h3 className="easy"> easy</h3>
+            </div>
+            <div className="mediumLevel">
+                <h3 className="medium"> easy</h3>
+            </div>
+            <div className="advancedLevel">
+                <h3 className="advanced"> easy</h3>
+            </div>
+           
+        </div>
+        </section>
+    )
+}
+
+export default Level
