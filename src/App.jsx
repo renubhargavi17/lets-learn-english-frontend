8bc70fabe4d450d4e2a37a81c46e5f8bc88de055
bzhang98 <105823390+bzhang98@users.noreply.github.com>
2024-11-07 14:17:12 -0600

    update routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.scss";
 import Language from "./components/Language/Language";
 import Level from "./components/Level/Level.jsx";
+import Quiz from "./components/Quiz/Quiz.jsx";
 import Header from "./components/Header/Header.jsx";
 
 import { useState } from "react";
@@ -8,7 +9,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   const [language, setLanguage] = useState(null);
-  const [difficulty, setDifficulty] = useState(null);
+  const [level, setLevel] = useState(null);
 
   return (
     <BrowserRouter>
@@ -17,13 +18,10 @@ function App() {
 
         <Routes>
           <Route path="/" element={<Language setLanguage={setLanguage} />} />
-          <Route
-            path="/difficulty"
-            element={<Language setLanguage={setLanguage} />}
-          />
+          <Route path="/difficulty" element={<Level setLevel={setLevel} />} />
           <Route
             path="/quiz"
-            element={<Language setLanguage={setLanguage} />}
+            element={<Quiz language={language} difficulty={level} />}
           />
         </Routes>
       </div>
