e0c622e2d0b4d64a1ee06c35e8f43a2dd6eb42cf
j1li <iotapsai@gmail.com>
2024-11-07 16:53:07 -0500

    update cards style

--- src/components/Language/Language.jsx
+++ src/components/Language/Language.jsx
@@ -1,67 +1,79 @@
 import React from "react";
-// import arabic from "../../assets/arabic.jpeg"
-// import bonjour from '../../assets/bonjour.jpeg'
-// import hola from "../../assets/hola.png"
-// import mandarin from "../../assets/mandarin.jpeg"
-import "../Language/Language.scss";
 import { useNavigate } from "react-router-dom";
-
-import arabic from "../../assets/logo/arabic.svg"
-import french from '../../assets/logo/french.svg'
-import spanish from "../../assets/logo/spanish.svg"
-import mandarin from "../../assets/logo/mandarin.svg"
-import "../Language/Language.scss"
-
+import arabic from "../../assets/logo/arabic.svg";
+import french from '../../assets/logo/french.svg';
+import spanish from "../../assets/logo/spanish.svg";
+import mandarin from "../../assets/logo/mandarin.svg";
+import "../Language/Language.scss";
 
 function Language({ setLanguage }) {
-  const navigate = useNavigate();
-  return (
-    <section className="languageTitlePage">
-      <h2 className="heading">WHAT LANGUAGE DO YOU SPEAK?</h2>
-      <div className="languages">
-        <div
-          className="french"
-          onClick={() => {
-            setLanguage("french");
-            navigate("/difficulty");
-          }}
-        >
-          {/* <img  src={bonjour} alt="Bonjour greetings" /> */}
-          <h3 className="frenchTitle">French</h3>
-        </div>
-        <div
-          className="spanish"
-          onClick={() => {
-            setLanguage("spanish");
-            navigate("/difficulty");
-          }}
-        >
-          {/* <img  src={hola} alt="Spanish greetings" /> */}
-          <h3 className="spanishTitle">Spanish</h3>
-        </div>
-        <div
-          className="arabic"
-          onClick={() => {
-            setLanguage("arabic");
-            navigate("/difficulty");
-          }}
-        >
-          {/* <img  src={arabic}  alt="Arabic greetings" /> */}
-          <h3 className="arabicTitle">Arabic</h3>
-        </div>
-        <div
-          className="mandarin"
-          onClick={() => {
-            setLanguage("mandarin");
-            navigate("/difficulty");
-          }}
-        >
-          {/* <img  src={mandarin} alt="Mandarin greetings" /> */}
-          <h3 className="mandarinTitle">Mandarin</h3>
-        </div>
-      </div>
-    </section>
-  );
+    const navigate = useNavigate();
+    return (
+        <section className="languageTitlePage">
+            <h2 className="heading">WHAT LANGUAGE DO YOU SPEAK?</h2>
+            <div className="languages">
+                <div
+                    className="languageCard french"
+                    onClick={() => {
+                        setLanguage("french");
+                        navigate("/difficulty");
+                    }}
+                >
+                    <div className="languageCard__image">
+                        <img src={french} alt="French greetings" />
+                    </div>
+                    <div className="languageCard__body">
+                        <h3>French</h3>
+                    </div>
+                </div>
+
+                <div
+                    className="languageCard spanish"
+                    onClick={() => {
+                        setLanguage("spanish");
+                        navigate("/difficulty");
+                    }}
+                >
+                    <div className="languageCard__image">
+                        <img src={spanish} alt="Spanish greetings" />
+                    </div>
+                    <div className="languageCard__body">
+                        <h3>Spanish</h3>
+                    </div>
+                </div>
+
+                <div
+                    className="languageCard arabic"
+                    onClick={() => {
+                        setLanguage("arabic");
+                        navigate("/difficulty");
+                    }}
+                >
+                    <div className="languageCard__image">
+                        <img src={arabic} alt="Arabic greetings" />
+                    </div>
+                    <div className="languageCard__body">
+                        <h3>Arabic</h3>
+                    </div>
+                </div>
+
+                <div
+                    className="languageCard mandarin"
+                    onClick={() => {
+                        setLanguage("mandarin");
+                        navigate("/difficulty");
+                    }}
+                >
+                    <div className="languageCard__image">
+                        <img src={mandarin} alt="Mandarin greetings" />
+                    </div>
+                    <div className="languageCard__body">
+                        <h3>Mandarin</h3>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 }
 
 export default Language;
