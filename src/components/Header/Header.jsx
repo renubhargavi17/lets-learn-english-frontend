8cf7425b32549cd0bc323de7bfe5da3754068db4
Renu Bhargavi Seetharaman <renubhargavi17@gmail.com>
2024-11-07 16:37:18 -0500

    logo added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import "../Header/Header.scss"
+import logo from "../../assets/logo/languages.svg"
 
 function Header(){
     return(
         <header className="header">
         <h1 className="title">LET'S LEARN ENGLISH</h1>
+        <img src={logo} alt="Let's Learn English Logo" className="letsLogo"/>
         
         </header>
     )
