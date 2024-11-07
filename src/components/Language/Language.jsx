81c5351c55afffa91a1d701c022ef537ea0418b8
Renu Bhargavi Seetharaman <renubhargavi17@gmail.com>
2024-11-07 16:09:53 -0500

    styling icon changes

--- src/components/Language/Language.jsx
+++ src/components/Language/Language.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-// import arabic from "../../assets/arabic.jpeg"
-// import bonjour from '../../assets/bonjour.jpeg'
-// import hola from "../../assets/hola.png"
-// import mandarin from "../../assets/mandarin.jpeg"
+import arabic from "../../assets/logo/arabic.svg"
+import french from '../../assets/logo/french.svg'
+import spanish from "../../assets/logo/spanish.svg"
+import mandarin from "../../assets/logo/mandarin.svg"
 import "../Language/Language.scss"
 
 function Language(){
@@ -11,19 +11,19 @@ return(
 <h2 className="heading">WHAT LANGUAGE DO YOU SPEAK?</h2>
     <div className="languages">
         <div className="french">
-        {/* <img  src={bonjour} alt="Bonjour greetings" /> */}
+        <img  src={french} alt="Bonjour greetings"  className="logo"/>
             <h3 className="frenchTitle">French</h3>
         </div>
         <div className="spanish">
-        {/* <img  src={hola} alt="Spanish greetings" /> */}
+        <img  src={spanish} alt="Spanish greetings" className="logo" />
             <h3 className="spanishTitle">Spanish</h3>
         </div>
         <div className="arabic">
-        {/* <img  src={arabic}  alt="Arabic greetings" /> */}
+        <img  src={arabic}  alt="Arabic greetings" className="logo" />
             <h3 className="arabicTitle">Arabic</h3>
         </div>
         <div className="mandarin">
-        {/* <img  src={mandarin} alt="Mandarin greetings" /> */}
+        <img  src={mandarin} alt="Mandarin greetings"className="logo" />
             <h3 className="mandarinTitle">Mandarin</h3>
         </div>
 
