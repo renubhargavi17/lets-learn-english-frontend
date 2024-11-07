c196d9b54e319f0b29b07a978abba3c370c2bd3c
bzhang98 <105823390+bzhang98@users.noreply.github.com>
2024-11-07 14:20:02 -0600

    reformat files

--- src/components/Language/Language.jsx
+++ src/components/Language/Language.jsx
@@ -3,33 +3,32 @@ import React from "react";
 // import bonjour from '../../assets/bonjour.jpeg'
 // import hola from "../../assets/hola.png"
 // import mandarin from "../../assets/mandarin.jpeg"
-import "../Language/Language.scss"
+import "../Language/Language.scss";
 
-function Language(){
-return(
-<section className="languageTitlePage">
-<h2 className="heading">WHAT LANGUAGE DO YOU SPEAK?</h2>
-    <div className="languages">
+function Language() {
+  return (
+    <section className="languageTitlePage">
+      <h2 className="heading">WHAT LANGUAGE DO YOU SPEAK?</h2>
+      <div className="languages">
         <div className="french">
-        {/* <img  src={bonjour} alt="Bonjour greetings" /> */}
-            <h3 className="frenchTitle">French</h3>
+          {/* <img  src={bonjour} alt="Bonjour greetings" /> */}
+          <h3 className="frenchTitle">French</h3>
         </div>
         <div className="spanish">
-        {/* <img  src={hola} alt="Spanish greetings" /> */}
-            <h3 className="spanishTitle">Spanish</h3>
+          {/* <img  src={hola} alt="Spanish greetings" /> */}
+          <h3 className="spanishTitle">Spanish</h3>
         </div>
         <div className="arabic">
-        {/* <img  src={arabic}  alt="Arabic greetings" /> */}
-            <h3 className="arabicTitle">Arabic</h3>
+          {/* <img  src={arabic}  alt="Arabic greetings" /> */}
+          <h3 className="arabicTitle">Arabic</h3>
         </div>
         <div className="mandarin">
-        {/* <img  src={mandarin} alt="Mandarin greetings" /> */}
-            <h3 className="mandarinTitle">Mandarin</h3>
+          {/* <img  src={mandarin} alt="Mandarin greetings" /> */}
+          <h3 className="mandarinTitle">Mandarin</h3>
         </div>
-
-    </div>
-</section>
-)
+      </div>
+    </section>
+  );
 }
 
-export default Language
+export default Language;
