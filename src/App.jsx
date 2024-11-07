48c3ecba5c9323a85dde37ce16a3f3dc4de154bc
Hana <117533065+hana-tak@users.noreply.github.com>
2024-11-07 12:14:00 -0800

    Revert "Feature/hana"

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,14 @@
 
-import './App.scss'
+import './App.css'
 import Language from './components/Language/Language'
-import Header from './components/Header/Header.jsx'
-import Level from './components/Level/Level.jsx'
-
-
-
-
 
 function App() {
  
 
   return (
- <div  className='learnLanguage'>
-    <Header/>
-
+    <>
       <Language/>
-      <Level/>
-      </div>
-  
-  
+    </>
   )
 }
 
--- src/components/Language/Language.jsx
+++ src/components/Language/Language.jsx
@@ -1,34 +1,13 @@
 import React from "react";
-// import arabic from "../../assets/arabic.jpeg"
-// import bonjour from '../../assets/bonjour.jpeg'
-// import hola from "../../assets/hola.png"
-// import mandarin from "../../assets/mandarin.jpeg"
-import "../Language/Language.scss"
-
 function Language(){
 return(
-<section className="languageTitlePage">
-<h2 className="heading">WHAT LANGUAGE DO YOU SPEAK?</h2>
-    <div className="languages">
-        <div className="french">
-        {/* <img  src={bonjour} alt="Bonjour greetings" /> */}
-            <h3 className="frenchTitle">French</h3>
-        </div>
-        <div className="spanish">
-        {/* <img  src={hola} alt="Spanish greetings" /> */}
-            <h3 className="spanishTitle">Spanish</h3>
-        </div>
-        <div className="arabic">
-        {/* <img  src={arabic}  alt="Arabic greetings" /> */}
-            <h3 className="arabicTitle">Arabic</h3>
-        </div>
-        <div className="mandarin">
-        {/* <img  src={mandarin} alt="Mandarin greetings" /> */}
-            <h3 className="mandarinTitle">Mandarin</h3>
-        </div>
+<>
+    <h1 className="title">LET'S LEARN ENGLISH</h1>
+    <h2 className="heading">WHAT LANGUAGE DO YOU SPEAK?</h2>
+    <div className="language">
 
     </div>
-</section>
+</>
 )
 }
 
